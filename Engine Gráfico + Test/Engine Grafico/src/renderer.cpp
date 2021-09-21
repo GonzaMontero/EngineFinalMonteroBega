37479c6c5271ae84a3fcf69b596f15ed0734c14f
Julian Bega <julian.begamilrad@gmail.com>
2021-09-21 00:47:19 -0300

    shader

--- Engine Gráfico + Test/Engine Grafico/src/renderer.cpp
+++ Engine Gráfico + Test/Engine Grafico/src/renderer.cpp
@@ -1,6 +1,89 @@
 #include "glew.h"
 #include "glfw3.h"
 #include "renderer.h"
+#pragma region shaders like cherno
+
+/*
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+		std::cout << "failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader" << std::endl;
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+}
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentSader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader );
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, vertexShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+*/
+
+#pragma endregion
+
+struct ShaderProgramSource
+{
+	string VertexSource;
+	string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const string& filepath)
+{
+	ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+
+	string line;
+	stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != string::npos)
+		{
+			if (line.find("vertex") != string::npos)
+			{
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != string::npos)
+			{
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			ss[(int)type] << line << '\n';
+		}
+	}
+
+	return { ss[VERTEX].str(), ss[FRAGMENT].str() };
+}
 
 static unsigned int CompileShader(unsigned int type, const string& source)
 {
@@ -52,11 +135,13 @@ static unsigned int CreateShader(const string& vertexShader, const string& fragm
 	//leer las funciones que empiezan con gl y hacer comentario de que hacen para tener a mano
 }
 
+
+
 Renderer::Renderer() {
 	i = 0;
 }
 Renderer::~Renderer() {
-
+	//glDeleteProgram(shader);
 }
 
 void Renderer::DrawTriangle(CVec4 pos1, CVec4 pos2, CVec4 pos3) {
@@ -75,7 +160,13 @@ void Renderer::DrawTriangle(CVec4 pos1, CVec4 pos2, CVec4 pos3) {
 	// el glVertexAttribPointer es muy importante ya que espeficia como leer el buffer, como interpretar la data que ponemos adentro del mismo
 	glVertexAttribPointer(0, 8, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(4 * sizeof(float))); //index, tamaño del VERTEX , tipo de dato, si queremos que se normalice, stride (cuanto tiene que recorrer hasta el siguiente VERTEX, no al siguiente Attribute), stride al siguiente atributo
 
-	unsigned int shader = CreateShader("res/shader/vertex.shader", "res/shader/fragment.shader");
+	ShaderProgramSource source = ParseShader("res/shader/BasicShader.shader");
+
+	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+
+	//unsigned int shader = CreateShader("res/shader/vertex.shader", "res/shader/fragment.shader");   /// solo va a funcionar si hacemos los archivos .shader con el codigo correspondiente
+
+
 
 	glUseProgram(shader);
 
