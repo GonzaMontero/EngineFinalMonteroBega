7bae5203ee3ea737aca87d5da4712851a41afd6e
Gonzalo Montero <gonmon654@gmail.com>
2021-09-28 20:58:19 -0300

    Added Include

--- Engine Gráfico + Test/Engine Grafico/src/Entity.cpp
+++ Engine Gráfico + Test/Engine Grafico/src/Entity.cpp
@@ -1,2 +1,25 @@
 #include "Entity.h"
 
+void Entity::SetPosition(float x, float y, float z) {
+	position.x = x;
+	position.y = y;
+	position.z = z;
+
+	translate;
+}
+void Entity::SetScale(float x, float y, float z) {
+	scalation.x = x;
+	scalation.y = y;
+	scalation.z = z;
+}
+void Entity::SetRotation(float x, float y, float z) {
+	rotation.x = x;
+	rotation.y = y;
+	rotation.z = z;
+}
+void Entity::Translate(float x, float y, float z)
+{
+	position.x += x;
+	position.y += y;
+	position.z += z;
+}
--- Engine Gráfico + Test/Engine Grafico/src/Entity.h
+++ Engine Gráfico + Test/Engine Grafico/src/Entity.h
@@ -1,6 +1,8 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include "glm.hpp"
+#include "gtc/matrix_transform.hpp"
 #include "Export.h"
 #include "renderer.h"
 
@@ -8,6 +10,19 @@ class ENGINE_API Entity {
 public:
 	Renderer* renderer;
 
+	glm::mat4 translate;
+	glm::mat4 rotate;
+	glm::mat4 scale;
+	glm::mat4 model;
+
+	glm::vec3 position;
+	glm::vec3 rotation;
+	glm::vec3 scalation;
+
+	void SetPosition(float x, float y, float z);
+	void SetScale(float x, float y, float z);
+	void SetRotation(float x, float y, float z);
+	void Translate(float x, float y, float z);
 };
 
 #endif // !ENTITY_H
