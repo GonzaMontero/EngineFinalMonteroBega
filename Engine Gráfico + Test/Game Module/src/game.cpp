ed970bef1332ed6796c75f77cda652f84f270e41
Julian Bega <julian.begamilrad@gmail.com>
2021-11-18 10:18:07 -0300

    Fuciona con ortho

--- Engine Gráfico + Test/Game Module/src/game.cpp
+++ Engine Gráfico + Test/Game Module/src/game.cpp
@@ -14,59 +14,62 @@ void Game::init() {
 	valorEscala = 1;
 	testSprite = new Sprite(true, "res/meme2.png", &render);
 	testSprite->Init();
-	testSprite->SetScale(0.1, 0.1, -1);
-	testSprite->SetPosition(0, 0, 1);
+	testSprite->SetScale(80, 80, -1);
+	testSprite->SetPosition(300, 300, -1);
 
 	testSprite2 = new Sprite(false, "res/JPGExample.jpg", &render);
 	testSprite2->Init();
-	testSprite2->SetScale(0.1, 0.1, -1);
-	testSprite2->SetPosition(0, 1, 1);
+	testSprite2->SetScale(50, 50, -1);
+	testSprite2->SetPosition(250, 100, -1);
+	valorTranslateX = 250;
+	valorTranslateY = 100;
 }
 void Game::updateGame() {
 
 	testSprite->DrawSprite();
 	testSprite2->DrawSprite();
-	testSprite2->SetPosition(valorTranslateX, valorTranslateY, 1);
+	testSprite2->SetPosition(valorTranslateX, valorTranslateY, -1);
+	cout << "x:" << testSprite2->position.x << " y:" << testSprite2->position.y << " z:" << testSprite2->position.z << endl;
 	//testShape->SetRotation(0, 0, valorRotacion);
 	//testShape->SetPosition(valorTranslateX, valorTranslateY, 1);
 	//testShape->SetScale(valorEscala, valorEscala, valorEscala);
-	//testShape->Draw();
+    //testShape->Draw();
 	if(input.GetKey(KeyCode::M))
 	{
 	//	testShape->EpilepsyMode();
 	}
 	if (input.GetKey(KeyCode::Q))
 	{
-		valorRotacion += 0.01;
+		valorRotacion += 1;
 	}
 	if (input.GetKey(KeyCode::E))
 	{
-		valorRotacion -= 0.01;
+		valorRotacion -= 1;
 	}
 	if (input.GetKey(KeyCode::D))
 	{
-		valorTranslateX -= 0.01;
+		valorTranslateX += 1;
 	}
 	if (input.GetKey(KeyCode::A))
 	{
-		valorTranslateX += 0.01;
+		valorTranslateX -= 1;
 	}
 
 	if (input.GetKey(KeyCode::W))
 	{
-		valorTranslateY += 0.01;
+		valorTranslateY += 1;
 	}
 	if (input.GetKey(KeyCode::S))
 	{
-		valorTranslateY -= 0.01;
+		valorTranslateY -= 1;
 	}
 	if (input.GetKey(KeyCode::Z))
 	{
-		valorEscala -= 0.01;
+		valorEscala -= 1;
 	}
 	if (input.GetKey(KeyCode::X))
 	{
-		valorEscala += 0.01;
+		valorEscala += 1;
 	}
 	if (input.GetKey(KeyCode::R))
 	{
