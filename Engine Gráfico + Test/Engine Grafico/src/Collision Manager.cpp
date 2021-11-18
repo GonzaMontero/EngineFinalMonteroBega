b303a5f5a15efb33ac8f16e953e406248adb9d25
Gonzalo Montero <gonmon654@gmail.com>
2021-11-18 10:31:09 -0300

    Added AABB Collisions

--- Engine Gráfico + Test/Engine Grafico/src/Collision Manager.cpp
+++ Engine Gráfico + Test/Engine Grafico/src/Collision Manager.cpp
@@ -4,9 +4,19 @@ CollisionManager::CollisionManager(){
 
 }
 
-bool CollisionManager::CheckCollisions(Entity2D one, Entity2D two){
+bool CollisionManager::CheckAABBCollisions(Entity2D* one, Entity2D* two){
 
+	glm::vec2 oneScale(one->scalation.x, one->scalation.y);
+	glm::vec2 onePos(one->position.x, one->position.y);
 
+	glm::vec2 twoScale(two->scalation.x, two->scalation.y);
+	glm::vec2 twoPos(two->position.x, two->position.y);
 
-	return true;
+	bool collisionX = onePos.x + oneScale.x >= twoPos.x &&
+		twoPos.x + twoScale.x >= onePos.x;
+
+	bool collisionY = onePos.y + oneScale.y >= twoPos.y &&
+		twoPos.y + twoScale.y >= onePos.y;
+
+	return collisionX && collisionY;
 }
--- Engine Gráfico + Test/Engine Grafico/src/Collision Manager.h
+++ Engine Gráfico + Test/Engine Grafico/src/Collision Manager.h
@@ -3,12 +3,12 @@
 
 #include "Export.h"
 #include "Entity2D.h"
+#include "glm/glm.hpp"
 
 class ENGINE_API CollisionManager {
 public:
 	CollisionManager();
 	
-	bool CheckCollisions(Entity2D one, Entity2D two);
-
+	bool CheckAABBCollisions(Entity2D* one, Entity2D* two);
 };
 #endif // !CAMERA_H
