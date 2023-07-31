eb2ee2cf1eb216bea9fe33e3b4245d3455a9aca4
Julian Bega <julian.begamilrad@gmail.com>
2023-07-30 21:11:14 -0300

    Comentarios Collision Manager

--- Project/Engine Grafico/src/Collision/Collision Manager.cpp
+++ Project/Engine Grafico/src/Collision/Collision Manager.cpp
@@ -9,11 +9,12 @@ namespace Engine {
 
 	}
 
-	collisionPos CollisionManager::PartialCollision(Entity2D* entity, Entity2D* otherEntity) {
+	collisionPos CollisionManager::PartialCollision(Entity2D* entity, Entity2D* otherEntity) { //checkea colision entre 2 entidades
 		glm::vec2 entityScale(entity->transform.scale.x - 20, entity->transform.scale.y);
 		glm::vec2 obstacleScale(otherEntity->transform.scale.x, otherEntity->transform.scale.y);
 		glm::vec2 entityPos(entity->transform.position.x, entity->transform.position.y - 10);
 		glm::vec2 obstaclePos(otherEntity->transform.position.x, otherEntity->transform.position.y);
+		//Define si hay overlap
 		float minX = 0.0f;
 		float maxX = glm::min(entityPos.x + fabs(entityScale.x) / 2.0f, obstaclePos.x + fabs(obstacleScale.x) / 2.0f) - glm::max(entityPos.x - fabs(entityScale.x) / 2.0f, obstaclePos.x - fabs(obstacleScale.x) / 2.0f);
 		float overlapX = glm::max(minX, maxX);
@@ -21,7 +22,8 @@ namespace Engine {
 		float minY = 0.0f;
 		float maxY = glm::min(entityPos.y + fabs(entityScale.y) / 2.0f, obstaclePos.y + fabs(obstacleScale.y) / 2.0f) - glm::max(entityPos.y - fabs(entityScale.y) / 2.0f, obstaclePos.y - fabs(obstacleScale.y) / 2.0f);
 		float overlapY = glm::max(minY, maxY);
-
+		
+		//Si hay overlap, dependiendo de el tipo de overlap retorna el tipo de colision
 		if (overlapX != 0.0f && overlapY != 0.0f) {
 			if (overlapX > overlapY) {
 				if (obstaclePos.y < 0 && entityPos.y < obstaclePos.y || entityPos.y > 0 && entityPos.y < obstaclePos.y) {
@@ -43,7 +45,8 @@ namespace Engine {
 		return none;
 	}
 
-	bool CollisionManager::CheckTrigger(Entity2D* entity1, Entity2D* entity2) {
+	bool CollisionManager::CheckTrigger(Entity2D* entity1, Entity2D* entity2) { 
+		//Revisa si hay colision pero no hace nada mas, check colission se impide dicha colision, mientras que en este caso solo se retorna true o false
 		collisionPos cPosition = PartialCollision(entity1, entity2);
 		if (cPosition != none) {
 			return true;
@@ -52,6 +55,7 @@ namespace Engine {
 	}
 
 	bool CollisionManager::CheckCollision(Entity2D* entity1, Entity2D* entity2, float speedEntity1) {
+		//Revisa si hay una colision, y en caso de que la haya se devuelve a entity a su lugar previo a la colision
 		collisionPos cPosition = PartialCollision(entity1, entity2);
 		switch (cPosition) {
 		case none:
