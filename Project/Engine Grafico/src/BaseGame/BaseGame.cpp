cca56cb39e2eb88fea79a1cee02b3831acdb317f
Gonzalo Montero <gonmon654@gmail.com>
2024-07-01 18:08:54 -0300

    #0003 - Base Game Updates
    Updated BaseGame.h and BaseGame.cpp

--- Project/Engine Grafico/src/BaseGame/BaseGame.cpp
+++ Project/Engine Grafico/src/BaseGame/BaseGame.cpp
@@ -3,101 +3,225 @@
 
 #include "BaseGame.h"
 
+#include <random>
+
 using namespace Engine;
 
-BaseGame::BaseGame() {
-	_renderer = new Renderer();
-	_window = new Window(1280, 720);
-	_camera = new Camera(_renderer, ProjectionType::orthographic);
-	_collisionManager = new CollisionManager();
+
+BaseGame::BaseGame() 
+{
+	_window = NULL;
+	_renderer = NULL;
+	_time = NULL;
+	_input = NULL;
+	_collisionManager = NULL;
 }
 
-BaseGame::~BaseGame() {
-	if (_renderer != NULL) {
-		delete _renderer;
-		_renderer = NULL;
-	}
-
+BaseGame::~BaseGame() 
+{
 	if (_window != NULL)
 	{
 		delete _window;
 		_window = NULL;
 	}
 
-	if (_camera != NULL) {
-		delete _camera;
-		_camera = NULL;
+	if (_renderer != NULL) 
+	{
+		delete _renderer;
+		_renderer = NULL;
 	}
 
-	if (_collisionManager != NULL) {
+	if(_time != NULL)
+	{
+		delete _time;
+		_time = NULL;
+	}
+
+	if (_input != NULL) 
+	{
+		delete _input;
+		_input = NULL;
+	}
+
+	if (_collisionManager != NULL) 
+	{
 		delete _collisionManager;
 		_collisionManager = NULL;
 	}
 }
 
-void BaseGame::Execute() {
-	Init();
-	Update();
-	Unload();
-}
 
-int BaseGame::Init() {
-	_window->CreateWindow("Engine Final Montero-Bega");
+bool Engine::BaseGame::Initialize(int windowWidth, int windowHeight, const char* windowName)
+{
+	if (!glfwInit())
+		return false;
 
-	if (!_renderer->InitializeGlew()) 
+	_window = new Window(windowWidth, windowHeight, windowName);
+
+	if (_window->GetWindow())
 	{
-		return 0;
+		glfwTerminate();
+		delete _window;
+		return false;
 	}
 
-	basicShader.Create("..//Engine Grafico//src//Shader//shader//vertex.vert", "..//Engine Grafico//src//Shader//shader//fragment.frag");
-	textureShader.Create("..//Engine Grafico//src//Shader//shader//texture_vert.vert", "..//Engine Grafico//src//Shader//shader//texture_frag.frag");
+	_window->InitWindow();
 
-	glEnable(GL_DEPTH_TEST);
-
-	_camera->transform.position = glm::vec3(0.0f, 0.0f, -3.0f);
-
-	_camera->SetView(glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-	_camera->SetProjection(ProjectionType::orthographic);
-
-	_camera->Init(basicShader);
-	_camera->Init(textureShader);
-
-
-	input.SetWindow(_window->GetWindow());
-
-	time.Reset();
-
-	InitGame();
-}
-
-void BaseGame::Update() {
-	float speed = 0.01f;
-
-	while (!glfwWindowShouldClose(_window->GetWindow())) 
+	if (glewInit() != GLEW_OK) //Check if GLEW Init is successful, checked after GLFW due to being Context Aware
 	{
-		_renderer->BeginFrame(0.0f, 0.0f, 0.0f);
-
-		UpdateGame();
-
-		_camera->Draw(basicShader);
-		_camera->Draw(textureShader);
-
-		time.CalculateFPS();
-		time.Tick();
-
-		_renderer->EndFrame(_window->GetWindow());
+		std::cout << "GLEW Error" << std::endl;
+		return false;
 	}
+
+	_renderer = new Renderer(_window);
+
+	_time = new Time();
+
+	_input = new Input(_window);
+
+	_collisionManager = new CollisionManager();
+
+	return true;
 }
 
-void BaseGame::Unload() {
-	UnloadGame();
+void Engine::BaseGame::Denitialize()
+{
 	glfwTerminate();
 }
 
-Renderer* BaseGame::GetRenderer() {
-	return _renderer;
+bool Engine::BaseGame::WindowExitCheck()
+{
+	return glfwWindowShouldClose(_window->GetWindow());
 }
 
-Window* BaseGame::GetWindow() {
-	return _window;
+void Engine::BaseGame::InitEngine(int windowWidth, int windowHeight, const char* windowName)
+{
+	if (Initialize(windowWidth, windowHeight, windowName)) 
+	{
+		Init();
+
+		while (!WindowExitCheck())
+		{
+			glfwPollEvents();
+
+			_time->UpdateDeltaTime(GetCurrentTime());
+
+			Update();
+
+			_renderer->StartDraw();
+
+			Draw();
+
+			_renderer->EndDraw();
+		}
+
+		DeInit();
+
+		Denitialize();
+	}
 }
+
+bool Engine::BaseGame::IsKeyPressed(int keycode)
+{
+	return _input -> IsKeyPressed(keycode);
+}
+
+bool Engine::BaseGame::IsKeyDown(int keycode)
+{
+	return _input -> IsKeyDown(keycode);
+}
+
+glm::vec2 Engine::BaseGame::GetMousePosition()
+{
+	return _input -> GetMousePosition();
+}
+
+void Engine::BaseGame::ChangeClearColor(glm::vec4 color)
+{
+	_renderer->SetColor(color);
+}
+
+double Engine::BaseGame::GetCurrentTime()
+{
+	return glfwGetTime();
+}
+
+float Engine::BaseGame::Lerp(float v0, float v1, float t)
+{
+	return 0.0f;
+}
+
+float Engine::BaseGame::Lerp(glm::vec4 color1, glm::vec4 color2, float t)
+{
+	return 0.0f;
+}
+
+float Engine::BaseGame::GetRandomNumber(float min, float max)
+{
+	random_device randDevice; //Using random device since rand() is limited to UINT_MAX
+	std::mt19937 mt(randDevice()); //A Mersenne Twister pseudo-random generator of 32-bit numbers with a state size of 19937 bits (1)
+	std::uniform_real_distribution<float> dist(min, max);
+	return dist(mt);
+}
+
+glm::vec4 Engine::BaseGame::GetRandomColor()
+{
+	float r = GetRandomNumber(0, 1);
+	float g = GetRandomNumber(0, 1);
+	float b = GetRandomNumber(0, 1);
+	float a = GetRandomNumber(0, 1);
+
+	return glm::vec4(r, g, b, a);
+}
+
+void Engine::BaseGame::AttachCollider(Entity2D* entity, bool isStatic)
+{
+	_collisionManager->AddToCollision(entity, isStatic);
+}
+
+void Engine::BaseGame::RemoveCollider(Entity2D* entity)
+{
+	_collisionManager->RemoveFromCollision(entity);
+}
+
+bool Engine::BaseGame::ContainsCollider(Entity2D* entity)
+{
+	return _collisionManager->IsEntityOnCollisions(entity);
+}
+
+void Engine::BaseGame::UpdateCollisionsTilemap(Tilemap* tileMap)
+{
+	_collisionManager->UpdateCollisionsOnMap();
+}
+
+//int BaseGame::Init() {
+//	_window->CreateWindow("Engine Final Montero-Bega");
+//
+//	if (!_renderer->InitializeGlew()) 
+//	{
+//		return 0;
+//	}
+//
+//	basicShader.Create("..//Engine Grafico//src//Shader//shader//vertex.vert", "..//Engine Grafico//src//Shader//shader//fragment.frag");
+//	textureShader.Create("..//Engine Grafico//src//Shader//shader//texture_vert.vert", "..//Engine Grafico//src//Shader//shader//texture_frag.frag");
+//
+//	glEnable(GL_DEPTH_TEST);
+//
+//	_camera->transform.position = glm::vec3(0.0f, 0.0f, -3.0f);
+//
+//	_camera->SetView(glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+//	_camera->SetProjection(ProjectionType::orthographic);
+//
+//	_camera->Init(basicShader);
+//	_camera->Init(textureShader);
+//
+//
+//	input.SetWindow(_window->GetWindow());
+//
+//	time.Reset();
+//
+//	InitGame();
+//}
+
+// (1) - Source: https://cplusplus.com/reference/random/mt19937/
+// (2) - Source: https://en.cppreference.com/w/cpp/numeric/random/uniform_real_distribution
--- Project/Engine Grafico/src/BaseGame/BaseGame.h
+++ Project/Engine Grafico/src/BaseGame/BaseGame.h
@@ -18,35 +18,63 @@
 #include "../xml_lib/tinyxml2.h"
 
 namespace Engine {
+
+	class Window;
+	class Renderer;
+	class Input;
+	class CollisionManager;
+
 	class ENGINE_API BaseGame {
 	private:
-		Renderer* _renderer;
-		Window* _window;
+		//Internal functions to initialize all elements
+		bool Initialize(int windowWidth, int windowHeight, const char* windowName);
+		void Denitialize();
+		bool WindowExitCheck();
 
-	protected:
-		Camera* _camera;
-		Shader basicShader;
-		Shader textureShader;
-		Renderer* GetRenderer();
-		Window* GetWindow();
+		//Internal elements (not to be accessed by the end user)
+		Window* _window;
+		Input* _input;
+		Time* _time;
 		CollisionManager* _collisionManager;
 
-		Time time;
-		Input input;
-
-		int Init();
-		void Update();
-		void Unload();
+	protected:
+		Renderer* _renderer;
 
 	public:
 		BaseGame();
 		~BaseGame();
 
-		void Execute();
+		//External Functions which can be used by the end user
+		void InitEngine(int windowWidth, int windowHeight, const char* windowName);
 
-		virtual void InitGame() = 0;
-		virtual void UpdateGame() = 0;
-		virtual void UnloadGame() = 0;	
+		//Virtual functions which will allow the end user to override them and add code to be executed - I.E. adding draw calls 
+		//for Sprites
+		virtual void Init() = 0; //Initial function which will not only enable and create engine classes but also initialize end user
+									//classes
+		virtual void Update() = 0; //Updates all frames and is used by end user for movement, as an example
+		virtual void Draw() = 0; //Sends data for drawing all on screen elements, and the end user manually adds all foreign elements
+		virtual void DeInit() = 0; //Allows the end user to unload all data used during runtime
+
+		//Useful methods for the end user to perform - Input
+		bool IsKeyPressed(int keycode); //Pressed = Will return true every frame the key is pressed
+		bool IsKeyDown(int keycode); //Down = Will return true the first frame the key is pressed ONLY
+		glm::vec2 GetMousePosition(); //Returns the mouse position on screen
+
+		//Useful methods for the end user to perform - Config and Time
+		void ChangeClearColor(glm::vec4 color); //Swaps the color used during the screen clear to specified values
+		double GetCurrentTime(); //Returns time elapsed since GLFW initialized
+
+		//Useful methods for the end user to perform - Calculations and Random
+		float Lerp(float v0, float v1, float t); //Interpolates between two values based on T
+		float Lerp(glm::vec4 color1, glm::vec4 color2, float t); //Interpolates between two colors based on T
+		float GetRandomNumber(float min, float max); //Returns a random number between the min and max value parameters
+		glm::vec4 GetRandomColor(); //Returns a random color
+
+		//Useful methods for the end user to perform - Collision Calculations and Modifications
+		void AttachCollider(Entity2D* entity, bool isStatic); //Attaches a collider to a specific entity and if movement is possible
+		void RemoveCollider(Entity2D* entity); //Removes collider from specific entity, if possible
+		bool ContainsCollider(Entity2D* entity); //Checks if specified entity contains a collider
+		void UpdateCollisionsTilemap(Tilemap* tileMap); // Checks collisions on the specified tilemap
 	};
 }
 #endif // !BASEGAME_H
