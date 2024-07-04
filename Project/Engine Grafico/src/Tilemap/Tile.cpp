dfbd908431c280ff0fd2726309475234004f51cc
Gonzalo Montero <gonmon654@gmail.com>
2024-07-04 12:21:33 -0300

    0009 - Modified Tile
    Modified Tile.h and Tile.cpp in preparation for tilemap upgrades

--- Project/Engine Grafico/src/Tilemap/Tile.cpp
+++ Project/Engine Grafico/src/Tilemap/Tile.cpp
@@ -1,110 +1,40 @@
 #include "Tile.h"
-#include "../xml_lib/tinyxml2.h"
-#include <vector>
-#include <iostream>
 
-using namespace tinyxml2;
-using namespace Engine;
-
-Tile::Tile() : Sprite() {
+Engine::Tile::Tile()
+{
 	_id = 0;
-	_isWalkable = false;
-	_width = 0;
-	_height = 0;
+	_walkable = false;
 }
 
-Tile::Tile(unsigned int id, bool isWalkable) : Sprite() {
-	_id = id;
-	_isWalkable = isWalkable;
+Engine::Tile::Tile(int ID, bool walkable, Renderer* renderer, const char* imageFilePath, bool invertImage)
+{
+	_id = ID;
+	_walkable = walkable;
+
+	SetTexture(renderer, imageFilePath, invertImage);
 }
 
-Tile::Tile(unsigned int id, bool isWalkable, int width, int height, Renderer* renderer) : Sprite() {
-	_id = id;
-	_isWalkable = isWalkable;
-	_width = width;
-	_height = height;
-	_renderer = renderer;
-}
-
-Tile::Tile(unsigned int id, bool isWalkable, int width, int height, int posX, int posY, Renderer* renderer) : Sprite() {
-	_id = id;
-	_isWalkable = isWalkable;
-	_width = width;
-	_height = height;
-	_posX = posX;
-	_posY = posY;
-	_renderer = renderer;
-}
-
-Tile::~Tile() {
+Engine::Tile::~Tile()
+{
 
 }
 
-void Tile::SetID(unsigned int id) {
-	_id = id;
+bool Engine::Tile::GetWalkable()
+{
+	return _walkable;
 }
 
-void Tile::SetWidth(int width) {
-	_width = width;
+void Engine::Tile::SetWalkable(bool walkable)
+{
+	_walkable = walkable;
 }
 
-void Tile::SetHeight(int height) {
-	_height = height;
-}
-
-//Metodo para lo ultimo, ver que parametros le pasamos para setear la variable
-void Tile::SetIsWalkable(const char* path) {
-	tinyxml2::XMLDocument doc;
-
-	doc.LoadFile(path);
-
-	tinyxml2::XMLElement* tileset = doc.FirstChildElement("tileset");
-	if (tileset) {
-		for (tinyxml2::XMLNode* tile = tileset->FirstChildElement(); tile != NULL; tile = tile->NextSiblingElement()) {
-			if (tile->ToElement() && tile->ToElement()->IntAttribute("id") && tile->ToElement()->IntAttribute("id") == _id) {
-				tinyxml2::XMLElement* properties = tile->FirstChildElement("properties");
-				if (properties != NULL) {
-					tinyxml2::XMLElement* propertyElement = properties->FirstChildElement("property");
-					if (propertyElement != NULL)
-						_isWalkable = propertyElement->BoolAttribute("value");
-				}
-			}
-		}
-	}
-	doc.Clear();
-}
-
-void Tile::SetPosX(int posX) {
-	_posX = posX;
-}
-
-void Tile::SetPosY(int posY) {
-	_posY = posY;
-}
-
-unsigned int Tile::GetID() {
+unsigned int Engine::Tile::GetID()
+{
 	return _id;
 }
 
-int Tile::GetWidth() {
-	return _width;
-}
-
-int Tile::GetHeight() {
-	return _height;
-}
-
-bool Tile::GetIsWalkable() {
-	return _isWalkable;
-}
-
-int Tile::GetPosX() {
-	return _posX;
-}
-
-int Tile::GetPosY() {
-	return _posY;
-}
-
-void Tile::Draw() {
+void Engine::Tile::SetID(unsigned int ID)
+{
+	_id = ID;
 }
